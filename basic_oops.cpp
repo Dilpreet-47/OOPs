f34ba22ce33e57e6224160f40cf8c47b724e1be8
Dilpreet-47 <singh.dilpreet.flux@gmail.com>
2025-08-13 05:28:36 +0530

    added factorial

--- basic_oops.cpp
+++ basic_oops.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+class Student
+{
+public:
+    string name;
+    int rollno;
+    char section;
+};
 
 int main(){
+    Student s1;
+    int i ;
+    cout<<"Enter your name : "<<endl;
+    cin>>s1.name;
+    cin>>s1.rollno;
+    cin>>s1.section;
+    cout<<s1.name<<endl;
+    cout<<s1.rollno<<endl;
+    cout<<s1.section<<endl;
     
     return 0;
 }
